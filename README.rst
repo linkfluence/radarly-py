d086fe4b4e67e55cd82b69c5bb4f1b6c9ccbbdfc
Hugo Zanghi <hugo.zanghi@linkfluence.com>
2019-01-29 12:20:40 +0100

    release 1.0.9, can't reupload a package with the same name

--- README.rst
+++ README.rst
@@ -17,7 +17,7 @@ radarly-py
 
 
 :Author: Linkfluence
-:Version: 1.0.8
+:Version: 1.0.9
 
 
 
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 
 setup(
     name='radarly-py',
-    version='1.0.8',
+    version='1.0.9',
     description="Python client for Radarly API",
     long_description=readme(),
     author='Linkfluence',
